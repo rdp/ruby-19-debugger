a23e0802b9399e3e44b81431eade4985c86bd084
rocky <rockyb@rubyforge.org>
2013-01-19 09:27:07 -0500

    1.9.3 and 1.9.2 patches combined, broken out and utility programs for creating working with patches

---
+++ patches/scripts/git-setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+if [[ $# != 1 ]] ; then
+    echo 2>&1 <<EOF 
+Usage:
+  $0 *ruby-tar.gz*
+
+Untar ruby source and set it up for git.
+EOF
+    exit 1
+fi
+tar_file="$1"
+if [[ ! -r "$tar_file" ]]; then
+    echo "tar file: $tar_file does not exist"  2>&1
+    exit 2
+fi
+
+tar -xzf $tar_file || {
+    echo "Error untarring $ruby_name" 2>&1
+    exit 3
+}
+
+ruby_dir=$(basename $tar_file .tar.gz)
+rm -fr ruby_dir
+
+files="
+configure
+encdb.h
+golf_prelude.c
+id.h
+insns.inc
+insns_info.inc
+known_errors.inc
+lex.c
+miniprelude.c
+newline.c
+node_name.inc
+opt_sc.inc
+optinsn.inc
+optunifs.inc
+parse.c
+parse.h
+revision.h
+transdb.h
+vm.inc
+vmtc.inc
+"
+
+git_dir="${ruby_dir}-git"
+mv -v ${ruby_dir} ${git_dir}
+cd ${git_dir} || {
+    error >2 "Error in cd to ${git_dir}"
+    exit 4
+}
+git init .
+rm -fv $files
+git add *
+git commit -m'What we got to work with' .
+exit $?
---
+++ patches/scripts/tarball-create.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+function __FILE__ {
+    echo ${BASH_SOURCE[0]}
+}
+patch=${PATCH:-patch}
+file=$(__FILE__)
+dirname=${file%/*}
+
+if [[ $# != 1 ]] ; then
+    echo 2>&1 <<EOF 
+Usage:
+  $0 *ruby-tar.gz*
+
+Untar ruby source and set it up for git.
+EOF
+    exit 1
+fi
+tar_file="$1"
+if [[ ! -r "$tar_file" ]]; then
+    echo "tar file: $tar_file does not exist"  2>&1
+    exit 2
+fi
+
+ruby_dir=$(basename $tar_file .tar.gz)
+rm -fr $ruby_dir 2>/dev/null
+tar -xzf $tar_file || {
+    echo "Error untarring $ruby_name" 2>&1
+    exit 3
+}
+
+ruby_debugger_dir="${ruby_dir}-debugger"
+mv -v $ruby_dir $ruby_debugger_dir 
+(cd $ruby_debugger_dir && \
+    ${patch} -p1 < ${dirname}/../ruby-1.9.3-combined.patch && 
+    cd .. && tar -czf ${ruby_debugger_dir}.tar.gz $ruby_debugger_dir)
+rc=$?
+if (( $rc == 0 ) ; then
+    rm -fr $ruby_debugger_dir
+exit $?
